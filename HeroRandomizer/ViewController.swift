966fc4e6356a41ebfca08c6644bf5618ddb1b870
Arychill <argyn.moldabek@gmail.com>
2025-03-07 06:08:18 +0500

    Initial Commit

---
+++ HeroRandomizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeroRandomizer
+//
+//  Created by Аргын Молдабек on 07.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
